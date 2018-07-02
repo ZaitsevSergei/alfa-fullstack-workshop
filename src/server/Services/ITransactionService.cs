4c89c423a1eb657d1a11da18583432fbc49c5805
Irina-PC\Sergey <za9csergey@yandex.ru>
2018-07-02 15:41:03 +0300

    TransactionService interface developed

---
+++ src/server/Services/ITransactionService.cs
@@ -0,0 +1,25 @@
+﻿using Server.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Services
+{
+    public interface ITransactionService
+    {
+        /// <summary>
+        /// Award 10 bonus rubles to card
+        /// </summary>
+        /// <param name="card">card to award</param>
+        void AwardBonus(CardService card);
+
+        /// <summary>
+        /// Currency exchange method
+        /// </summary>
+        /// <param name="moneytoExchange">money to exchange in currencyTaget</param>
+        /// <param name="currencyTarget">currnecy to exchange</param>
+        /// <returns>money value in currencyTarget</returns>
+        decimal CurrencyExchange(Money moneytoExchange, CurrencyType currencyTarget);
+
+    }
+}
---
+++ src/server/Services/TransactionService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Server.Infrastructure;
+
+namespace Server.Services
+{
+    public class TransactionService : ITransactionService
+    {
+
+        /// <summary>
+        /// Award 10 bonus rubles to card
+        /// </summary>
+        /// <param name="card">card to award</param>
+        public void AwardBonus(CardService card)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CurrencyExchange(Money moneytoExchange, CurrencyType currencyTarget)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
